224ef26a094f8ed52236f2bc9ba5be3af1915c14
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-22 22:09:39 -0600

    Add video source constants and update Duck Under page to include video for second step

--- src/app/takedowns/duck-under/duck-under.constants.ts
+++ src/app/takedowns/duck-under/duck-under.constants.ts
@@ -1,11 +1,12 @@
 const imgDomainSrc = process.env.NEXT_PUBLIC_IMAGE_DOMAIN
+const videoDomainSrc = process.env.NEXT_PUBLIC_VIDEO_DOMAIN
 
 export const FIRST_STEP_IMG_SRC = `${imgDomainSrc}/v1763867702/duck-under-1_rl7eud.webp`;
-export const SECOND_STEP_VIDEO_SRC = `${imgDomainSrc}/v1763868078/under-hook-2_musuf3.webm`;
-export const THIRD_STEP_IMG_SRC = `${imgDomainSrc}/v1763868191/under-hook-3_fblw2f.webp`;
-export const FOURTH_STEP_VIDEO_SRC = `${imgDomainSrc}/v1763869732/under-hook-4_rgr3fn.webm`;
-export const FIFTH_STEP_VIDEO_SRC = `${imgDomainSrc}/v1763869731/under-hook-5_aclusy.webm`;
-export const SIXTH_STEP_VIDEO_SRC = `${imgDomainSrc}/v1763869731/under-hook-6_zfdlcn.webm`;
-export const SEVENTH_STEP_VIDEO_SRC = `${imgDomainSrc}/v1763869731/under-hook-7_aak6qt.webm`;
-export const EIGHTH_STEP_VIDEO_SRC = `${imgDomainSrc}/v1763869730/under-hook-8_bjm5jy.webm`;
-export const NINTH_STEP_VIDEO_SRC = `${imgDomainSrc}/v1763869730/under-hook-9_mgpesc.webm`;
+export const SECOND_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763868078/under-hook-2_musuf3.webm`;
+export const THIRD_STEP_IMG_SRC = `${videoDomainSrc}/v1763868191/under-hook-3_fblw2f.webp`;
+export const FOURTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869732/under-hook-4_rgr3fn.webm`;
+export const FIFTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869731/under-hook-5_aclusy.webm`;
+export const SIXTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869731/under-hook-6_zfdlcn.webm`;
+export const SEVENTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869731/under-hook-7_aak6qt.webm`;
+export const EIGHTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869730/under-hook-8_bjm5jy.webm`;
+export const NINTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869730/under-hook-9_mgpesc.webm`;
--- src/app/takedowns/duck-under/page.tsx
+++ src/app/takedowns/duck-under/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import { FIRST_STEP_IMG_SRC } from "./duck-under.constants";
+import { FIRST_STEP_IMG_SRC, SECOND_STEP_VIDEO_SRC } from "./duck-under.constants";
 
 export default function DuckUnderPage() {
   return (
     <main className="min-h-screen p-4 flex flex-col items-center gap-8">
       <h1 className="text-4xl font-bold">Duck under</h1>
-      <a href="https://www.youtube.com/watch?v=Ww-SsGOHemY&t=41s">Video instruction</a>
+      <a href="https://www.youtube.com/watch?v=Ww-SsGOHemY&t=41s" target="_blank" rel="noopener noreferrer">Video instruction</a>
       <section className="flex flex-col gap-4">
         <h2 className="text-3xl font-semibold">Steps:</h2>
         <ul className="list-disc flex flex-col gap-3">
@@ -17,9 +17,10 @@ export default function DuckUnderPage() {
             alt="First step of duck under"
             width={605}
             height={530}
-            className="w-full md:aspect-square md:object-cover"
+            className="md:aspect-square md:object-cover w-72 h-72"
           />
           <li>With your other arm, put your palm on his bicep pushing his arm away</li>
+          <video src={SECOND_STEP_VIDEO_SRC} controls muted autoPlay loop className="md:aspect-square md:object-cover w-72 h-72" />
           <li>Use push pull theory</li>
           <li>Getting your head past their armpit is imperative</li>
         </ul>
