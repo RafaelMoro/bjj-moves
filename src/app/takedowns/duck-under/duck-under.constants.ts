dbcf7551e4735a87e620840c6dc2000254c92844
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-22 22:21:18 -0600

    Fix image source for third step and update Duck Under page to include additional video steps

--- src/app/takedowns/duck-under/duck-under.constants.ts
+++ src/app/takedowns/duck-under/duck-under.constants.ts
@@ -3,7 +3,7 @@ const videoDomainSrc = process.env.NEXT_PUBLIC_VIDEO_DOMAIN
 
 export const FIRST_STEP_IMG_SRC = `${imgDomainSrc}/v1763867702/duck-under-1_rl7eud.webp`;
 export const SECOND_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763868078/under-hook-2_musuf3.webm`;
-export const THIRD_STEP_IMG_SRC = `${videoDomainSrc}/v1763868191/under-hook-3_fblw2f.webp`;
+export const THIRD_STEP_IMG_SRC = `${imgDomainSrc}/v1763868191/under-hook-3_fblw2f.webp`;
 export const FOURTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869732/under-hook-4_rgr3fn.webm`;
 export const FIFTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869731/under-hook-5_aclusy.webm`;
 export const SIXTH_STEP_VIDEO_SRC = `${videoDomainSrc}/v1763869731/under-hook-6_zfdlcn.webm`;
