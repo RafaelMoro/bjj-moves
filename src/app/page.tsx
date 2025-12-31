7ad060fbb5717ddbebc11778b1f84feba48f377c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-12-31 09:51:52 -0600

    Add transition links and suggestions for techniques in daily notes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import Link from 'next/link'
-import { DAILY_DECEMBER_ROUTE, DUCK_UNDER_ROUTE, PASSING_GUARD_ROUTE } from "@/shared/constants/global.constants";
+import { DAILY_DECEMBER_ROUTE, DUCK_UNDER_ROUTE, PASSING_GUARD_ROUTE, TRANSITION_BREAKING_GUARD_LINK, TRANSITION_SIDECONTROL_NORTHSOUTH_LINK, TRANSITION_SWEEP_MOUNT_CLOSED_GUARD_LINK } from "@/shared/constants/global.constants";
 
 export const metadata: Metadata = {
   title: 'BJJ Basics - Home',
@@ -33,6 +33,55 @@ export default function MainPage() {
           <nav>
             <Link className="underline" href={DAILY_DECEMBER_ROUTE}>Dec 2025</Link>
           </nav>
+        </article>
+        <article className="flex flex-col gap-3">
+          <h4 className="text-xl font-semibold">Things to try</h4>
+          <ul>
+            <li>Try armbar from mount</li>
+            <li>Try guillotine</li>
+            <li>Try darce choke from north south</li>
+            <li>Transition from mount to back mount</li>
+          </ul>
+          <article>
+            <h4>Go from sidecontrol to north south</h4>
+            <a href={TRANSITION_SIDECONTROL_NORTHSOUTH_LINK}>Side control to north south link</a>
+            <ul>
+              <li>Initialize from side control with your collarbone pushing on his ribs</li>
+              <li>Note that you need your leg under his arm</li>
+              <li>Pass your north side arm that wraps under his neck to move the arm over his neck</li>
+              <li>Drop your hips so his arm trapped ir near his head</li>
+              <li>Extend your legs and drop your hips over his arm</li>
+              <li>Then start transitioning into north south with the south arm on his hips to avoid the opponent to turn</li>
+            </ul>
+          </article>
+
+          <article>
+            <h4>Pass the guard</h4>
+            <a href={TRANSITION_BREAKING_GUARD_LINK}>Breaking guard link</a>
+            <ul>
+              <li>Position your body as away as possible with your back aligned (espalda derechita)</li>
+              <li>Put your strong leg between his legs</li>
+              <li>Pin and stretch to break closed guard</li>
+              <li>Pin one leg to the mat and be aware of the other leg, underhook it may be an option</li>
+              <li>Start passing with the leg next the opponent</li>
+              <li>move your grips into the head and shoulders</li>
+              <li>Use your other leg to stretch it and stabilize the transition to finally pass your leg trapped into side control</li>
+            </ul>
+          </article>
+
+          <article>
+            <h4>Roll from mount to closed guard</h4>
+            <a href={TRANSITION_SWEEP_MOUNT_CLOSED_GUARD_LINK}>Roll from mount to closed guard link</a>
+            <ul>
+              <li>Me being on bottom</li>
+              <li>Take one arm and pass his collarbone, his chin and wrap his arm with your armpit</li>
+              <li>With your arm wrapping his arm, apply downward pressure into his elbow to avoid the opponent to post</li>
+              <li>Use your leg of the side of the arm pushing his leg into our arm trapped</li>
+              <li>Chop your arm into his armpit with an angle of 11 or 10. Your head being 12 and your legs being 6</li>
+              <li>Bridge off the opposite foot</li>
+              <li>Just roll until you are in his closed guard</li>
+            </ul>
+          </article>
         </article>
       </section>
       <section className="flex flex-col gap-5">
