0453d7151260fc1e64d9ea8c23d9be9ad9a82715
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-02 23:48:23 -0600

    Add images for each step of the Triangle Choke from Mount technique

--- src/app/basics/mount/triangle-choke/page.tsx
+++ src/app/basics/mount/triangle-choke/page.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+
 import { ARM_TRIANGLE_FROM_MOUNT_TIKTOK, TRIANGLE_CHOKE_SETUP_FROM_MOUNT_YT } from "@/shared/constants/global.constants";
 import { RiArrowLeftLine } from "@remixicon/react";
-import Link from "next/link";
+import { EIGHTH_STEP_IMG_SRC, ELEVENTH_STEP_IMG_SRC, FIFTH_STEP_IMG_SRC, FIRST_STEP_IMG_SRC, FOURTH_STEP_IMG_SRC, NINTH_STEP_IMG_SRC, SECOND_STEP_IMG_SRC, SEVENTH_STEP_IMG_SRC, SIXTH_STEP_IMG_SRC, TENTH_STEP_IMG_SRC, THIRD_STEP_IMG_SRC } from "./triangle-choke.constants";
 
 export default function TriangleChokeFromMountPage() {
   return (
@@ -24,15 +27,101 @@ export default function TriangleChokeFromMountPage() {
             <h3 className="text-3xl font-bold mb-5">Steps:</h3>
             <ol className="list-decimal pl-6 flex flex-col gap-3">
               <li>From mount, Pin opponent&apos;s arm to the mat using your both hands and grabbing his wrist</li>
+              <img
+                src={FIRST_STEP_IMG_SRC}
+                alt="First step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>Go for the crossface and elevating his arm a little</li>
+              <img
+                src={SECOND_STEP_IMG_SRC}
+                alt="Second step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>While pushing his arm up, have a fist being put in his armpit</li>
+              <img
+                src={THIRD_STEP_IMG_SRC}
+                alt="Third step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>One you are sure that your fist is on his armpit, post with the two hands using your fists</li>
+              <img
+                src={FOURTH_STEP_IMG_SRC}
+                alt="Fourth step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>Slide one leg behind his back and get your knee under his armpit</li>
+              <img
+                src={FIFTH_STEP_IMG_SRC}
+                alt="Fifth step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>Immediately grab your ankle behind his shoulder</li>
+              <img
+                src={SIXTH_STEP_IMG_SRC}
+                alt="Sixth step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>Lean forward posting and get your knee behind his neck</li>
+              <img
+                src={SEVENTH_STEP_IMG_SRC}
+                alt="Seventh step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>Lock your anckle with your other leg to make the triangle</li>
+              <img
+                src={EIGHTH_STEP_IMG_SRC}
+                alt="Eighth step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>Start siting and then go to your back grabbing one leg</li>
+              <img
+                src={NINTH_STEP_IMG_SRC}
+                alt="Ninth step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+              <img
+                src={TENTH_STEP_IMG_SRC}
+                alt="Tenth step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
+
               <li>When being on that position, to make the triangle tighter, grab his wrist and put his elbow to the floor</li>
+              <img
+                src={ELEVENTH_STEP_IMG_SRC}
+                alt="Eleventh step of triangle choke from mount"
+                width={605}
+                height={530}
+                className="md:aspect-square md:object-cover w-72 h-72"
+              />
             </ol>
           </section>
       </main>
---
+++ src/app/basics/mount/triangle-choke/triangle-choke.constants.ts
@@ -0,0 +1,13 @@
+const imgDomainSrc = process.env.NEXT_PUBLIC_IMAGE_DOMAIN
+
+export const FIRST_STEP_IMG_SRC = `${imgDomainSrc}/v1767418725/trianlge-choke-1_eplejk.webp`
+export const SECOND_STEP_IMG_SRC = `${imgDomainSrc}/v1767418724/triangle-choke-2_fy7p7m.webp`
+export const THIRD_STEP_IMG_SRC = `${imgDomainSrc}/v1767418723/triangle-choke-3_o8xsn7.webp`
+export const FOURTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418724/triangle-choke-4_kbqbef.webp`
+export const FIFTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418723/traingle-choke-5_uqbjqc.webp`
+export const SIXTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418723/triangle-choke-6_xilqe8.webp`
+export const SEVENTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418723/triangle-choke-7_suatu3.webp`
+export const EIGHTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418722/triangle-choke-8_xtxtau.webp`
+export const NINTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418723/triangle-choke-9_ghypvp.webp`
+export const TENTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418722/triangle-choke-10_mpilv0.webp`
+export const ELEVENTH_STEP_IMG_SRC = `${imgDomainSrc}/v1767418722/triangle-choke-11_kihl0o.webp`
