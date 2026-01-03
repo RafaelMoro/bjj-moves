8c3b1f68992d5433234cddf34dc33f165dc31366
Rafael Moro <rafael.moro.galindo@gmail.com>
2026-01-03 00:15:50 -0600

    Refactor Daily notes page layout and improve content structure

--- src/app/daily/dec-2025/page.tsx
+++ src/app/daily/dec-2025/page.tsx
@@ -1,3 +1,4 @@
+import { RiArrowLeftLine } from "@remixicon/react";
 import { Metadata } from "next";
 import Link from "next/link";
 
@@ -8,77 +9,109 @@ export const metadata: Metadata = {
 
 export default function DailyDec2025Page() {
   return (
-    <main className="min-h-screen p-4 flex flex-col items-center gap-8">
-      <Link href="/">Go back</Link>
-      <h1 className="text-4xl font-bold">Daily notes December 2025</h1>
+    <div className="min-h-screen p-4">
+      <header className="grid grid-cols-3 gap-4">
+        <Link href="/">
+          <RiArrowLeftLine />
+        </Link>
+        <h1 className="text-4xl font-bold">Daily notes December 2025</h1>
+      </header>
 
-      <article className="flex flex-col gap-5">
-        <h4 className="text-2xl font-bold">Monday, December 22th</h4>
-
-        <div className="flex flex-col gap-4">
-          <p className="text-xl font-semibold">What did we do well?</p>
-          <ul className="list-disc list-inside">
-            <li>Transition from side control to kesa gatame</li>
-            <li>Transition from side control to north south</li>
-          </ul>
-        </div>
-
-        <div className="flex flex-col gap-4">
-          <p className="text-xl font-semibold">What needs improvement?</p>
-          <ul className="list-disc list-inside">
-            <li>Isolate the arm in kesa gatame</li>
-            <li>Being able to secure the arms in side control, kesa gatame and north south</li>
-            <li>Improve escape while being in opponent&apos;s closed guard</li>
-            <li>Close contact in open guard</li>
-            <li>While passing guard, enter with the knee and lower your body</li>
-          </ul>
-        </div>
-      </article>
-
-      <hr />
-
-      <article className="flex flex-col gap-5">
-        <h4 className="text-2xl font-bold">Tuesday, December 23th</h4>
-
-        <div className="flex flex-col gap-4">
-          <p className="text-xl font-semibold">What did we do well?</p>
-          <ul className="list-disc list-inside">
-            <li>Defend americana</li>
-            <li>Defend armbar</li>
-          </ul>
-        </div>
-
-        <div className="flex flex-col gap-4">
-          <p className="text-xl font-semibold">What needs improvement?</p>
-          <ul className="list-disc list-inside">
-            <li>I got my guard passed down</li>
-            <ul className="list-disc pl-4 list-inside">
-              <li>I didn&apos;t put my foot in my opponent&apos;s hips</li>
-              <li>Always bring your legs into your opponent</li>
-            </ul>
-            <li>The opponent passed from side control to mount</li>
-            <li>I couldn&apos;t pass the opponent&apos;s guard and finished on triangles or armbars</li>
-            <li>I couldn&apos;t finished the darce choke from north south</li>
-            <li>I didn&apos;t finished the americana or armbar in kesa gatame</li>
-          </ul>
-        </div>
-      </article>
-
-      <section>
-        <h2 className="text-3xl font-bold">Next things to try</h2>
-
+      <main className="mt-10 flex flex-col items-center gap-8">
         <article className="flex flex-col gap-5">
-          <h4 className="text-2xl font-bold">Go from side control to north south</h4>
+          <h4 className="text-2xl font-bold">Monday, December 22th</h4>
 
           <div className="flex flex-col gap-4">
-            <p className="text-xl font-semibold">Steps</p>
+            <p className="text-xl font-semibold">What did we do well?</p>
             <ul className="list-disc list-inside">
-              <li>...</li>
-              <li>...</li>
+              <li>Transition from side control to kesa gatame</li>
+              <li>Transition from side control to north south</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <p className="text-xl font-semibold">What needs improvement?</p>
+            <ul className="list-disc list-inside">
+              <li>Isolate the arm in kesa gatame</li>
+              <li>Being able to secure the arms in side control, kesa gatame and north south</li>
+              <li>Improve escape while being in opponent&apos;s closed guard</li>
+              <li>Close contact in open guard</li>
+              <li>While passing guard, enter with the knee and lower your body</li>
             </ul>
           </div>
         </article>
-      </section>
-    </main>
+
+        <hr />
+
+        <article className="flex flex-col gap-5">
+          <h4 className="text-2xl font-bold">Tuesday, December 23th</h4>
+
+          <div className="flex flex-col gap-4">
+            <p className="text-xl font-semibold">What did we do well?</p>
+            <ul className="list-disc list-inside">
+              <li>Defend americana</li>
+              <li>Defend armbar</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <p className="text-xl font-semibold">What needs improvement?</p>
+            <ul className="list-disc list-inside">
+              <li>I got my guard passed down</li>
+              <ul className="list-disc pl-4 list-inside">
+                <li>I didn&apos;t put my foot in my opponent&apos;s hips</li>
+                <li>Always bring your legs into your opponent</li>
+              </ul>
+              <li>The opponent passed from side control to mount</li>
+              <li>I couldn&apos;t pass the opponent&apos;s guard and finished on triangles or armbars</li>
+              <li>I couldn&apos;t finished the darce choke from north south</li>
+              <li>I didn&apos;t finished the americana or armbar in kesa gatame</li>
+            </ul>
+          </div>
+        </article>
+
+        <article className="flex flex-col gap-5">
+          <h4 className="text-2xl font-bold">Wednesday, December 31st</h4>
+
+          <div className="flex flex-col gap-4">
+            <p className="text-xl font-semibold">What did we do well?</p>
+            <ul className="list-disc list-inside">
+              <li>My first submission from back mount with Rear naked choke</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <p className="text-xl font-semibold">What did we saw?</p>
+            <ul className="list-disc list-inside">
+              <li>5 steps of the mount</li>
+              <li>Americana with armbar from side control</li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <p className="text-xl font-semibold">What needs improvement?</p>
+            <ul className="list-disc list-inside">
+              <li>I gassed out</li>
+              <li>I let my guard being passed down</li>
+              <ul className="list-disc pl-4 list-inside">
+                <li>I didn&apos;t put my foot in my opponent&apos;s hips</li>
+                <li>Always bring your legs into your opponent</li>
+              </ul>
+              <li>I couldn&apos;t pass the opponent&apos;s guard and finished on triangles or armbars</li>
+            </ul>
+          </div>
+        </article>
+
+        <section className="flex flex-col gap-5">
+          <h2 className="text-3xl font-bold">Next things to try</h2>
+          <ul className="flex flex-col gap-2">
+            <li>Try armbar from mount</li>
+            <li>Try guillotine</li>
+            <li>Try darce choke from north south</li>
+            <li>Transition from mount to back mount</li>
+          </ul>
+        </section>
+      </main>
+    </div>
   )
 }
